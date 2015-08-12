e0c9db6631817f1419c9d580aa4a3a0165a37f26
Aneez91 <aneezjos19@gmail.com>
2015-08-12 23:02:39 +0530

    Create Soyuz11.java

---
+++ Soyuz11.java
@@ -0,0 +1,17 @@
+
+public class Soyuz11 {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int n = in.nextInt();
+	System.out.println("  /\\  ");
+	for(int i=1;i<=n;i++)
+		System.out.println("  ||  ");
+	System.out.println(" /||\\ ");
+	System.out.println("/:||:\\");
+	for(int i=1;i< n;i++)
+		 System.out.println("|:||:|");
+	System.out.println("|/||\\|");
+	System.out.println("  **  ");
+	System.out.println("  **  ");
+}
+}
