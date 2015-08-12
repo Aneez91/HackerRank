a42f268798e0010145f0ae28f05775c1872e1a1a
Aneez91 <aneezjos19@gmail.com>
2015-08-12 23:00:16 +0530

    Create AngryChildrenAgain.java

---
+++ AngryChildrenAgain.java
@@ -0,0 +1,31 @@
+
+public class AngryChildrenAgain {
+	 public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int n = in.nextInt(), k = in.nextInt();
+	        long[] x = new long[n];
+	        for(int i = 0; i < n; i++) x[i] = in.nextInt();
+	        Arrays.sort(x);
+	        System.out.println(f(n, k, x));
+	    }
+	    
+	    private static long f(int n, int k, long[] x){
+	        long t = unFair(k, x), min = t, s = 0;
+	        for(int i = 1; i < k; i++) s += x[i];
+	        
+	        for(int i = 1; i + k-1 < x.length; i++){
+	            t -= 2*s;
+	            if(t < min) min = t;
+	            s += x[i+k-1] -x[i];
+	        }
+	        return min;
+	    }
+	    
+	    private static long unFair(int k, long[] x){
+	        long s = 0;
+	        for(int i = 0; i < k; i++){
+	            s += x[i] *(2*i-k+1);
+	        }
+	        return s;
+	    }
+}
